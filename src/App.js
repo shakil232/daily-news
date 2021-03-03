846f7b0709eda75f59ec0a8b476902fec695f1ed
unknown <dxshakil2@gmail.com>
2021-03-03 00:40:33 -0800

    load na hole fragment check korte hve

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import './App.css';
 import Header from './Components/Header/Header';
+import ReactFragment from './Components/ReactFragment/ReactFragment';
 import TopHeader from './Components/TopHeader/TopHeader';
 
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div >
       <Header></Header>
+      <ReactFragment></ReactFragment>
       <TopHeader></TopHeader>
 
     </div>
---
+++ src/Components/ReactFragment/ReactFragment.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const ReactFragment = () => {
+    return (
+    <>
+      <h2> Fragment </h2>
+      <button> React-Fragment </button>
+    </>  
+    );
+};
+
+export default ReactFragment;
